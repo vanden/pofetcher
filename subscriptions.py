38824f14d44100bc83a2dc11b5bfe398f4b80130
Brian van den Broek <vanden@gmail.com>
2018-04-17 20:36:39 -0700

    Improve renamer routines

--- subscriptions.py
+++ subscriptions.py
@@ -14,7 +14,7 @@ class Subscription():
         self.log = log
         self.targetDir = targetDir
         self.count = count
-        self.renamer = renamer
+        self.renamer = renamer or Renamer().rename
 
 
     def parseFeed(self):
@@ -61,17 +61,28 @@ class Subscription():
 
 
 class Renamer():
-    def __init__(self, base):
+    def __init__(self, base=None):
         self.namesUsed = []
         self.base = base
 
     def rename(self, url, pubdatetime):
+        remoteFileName = url.rsplit('/', 1)[-1]
         date = pubdatetime.strftime("%Y%m%d")
         idn = 1
-        candidate = f"{self.base}_{date}_{idn:02}.mp3"
+
+        candidate = f"{remoteFileName}"
+        if self.base:
+            candidate = f"{self.base}_{date}_{idn:02}.mp3"
+        else:
+            base, ext = remoteFileName.rsplit('.', 1)
+
         while candidate in self.namesUsed:
             idn += 1
-            candidate = f"{self.base}_{date}_{idn:02}.mp3"
+            if self.base:
+                candidate = f"{self.base}_{date}_{idn:02}.mp3"
+            else:
+                candidate = f"{base}_{idn:02}.{ext}"
+
         self.namesUsed.append(candidate)
         return candidate
 
