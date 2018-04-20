489dd5a57e52941582728d130795b5a99c6e92a8
Brian van den Broek <vanden@gmail.com>
2018-04-19 21:17:40 -0700

    Add very rudimentatry readme

---
+++ README.rs
@@ -0,0 +1,9 @@
+A bespoke command line podfetcher
+
+It exists because I am quite fussy about naming conventions and
+directory structure for the podcasts that I download. I can't imagine
+this being of any real use or interest to anyone else. But, have at it.
+
+GPL v3.0 or later.
+
+Copyright 2018 Brian van den Broek 
