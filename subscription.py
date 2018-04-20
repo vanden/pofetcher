e9e2a703f81d4c5d8ea2bff705f89d20c6052163
Brian van den Broek <vanden@gmail.com>
2018-04-19 22:12:08 -0700

    Split monster method

--- subscription.py
+++ subscription.py
@@ -41,38 +41,58 @@ class Subscription():
                 # non-podcast items, too. Not sure what more if anything I
                 # should do. ThinkMore
                 continue
+
             if url in self.log or url in self.fetched:
                 continue
+
             if '?' in url:
+                # Some feeds have a '?' after the file extension leading off
+                # some metadata. Pull that all off to see if the file format
+                # is one we want.
                 realurl = url.split('?', 1)[0]
                 if not realurl.split('.')[-1] in ['mp3', 'ogg']:
                     print(''.join(
+                        # FixMe Isn't really the issue, is it?
                         ["Don't know how to parse",
                          f"{orig_url} from {self.name}; skipping"]))
-            print(f"\tFetching {url}")
-            dts = None
-            if 'published_parsed' in entry:
-                dts = entry.published_parsed
-            elif 'created_parsed' in entry:
-                dts = entry.created_parsed
-            if dts:
-                stamp = datetime.datetime(dts.tm_year, dts.tm_mon,
-                                          dts.tm_mday, dts.tm_hour,
-                                          dts.tm_min, dts.tm_sec)
-            else:
-                stamp = datetime.datetime.now()
 
+            print(f"\tFetching {url}")
+            self._fetchAndWrite(url, entry)
+
+
+    def _fetchAndWrite(self, url, entry):
+        localName = self._makeLocalName(url, entry)
+
+        r = requests.get(url, stream=True)
+        f = open(localName, 'wb')
+        for chunk in r.iter_content(chunk_size = 1024*4*4*4):
+            if chunk:
+                f.write(chunk)
+        f.close()
+        self.fetched.append(url)
+        self.log.update(url)
+
+
+    def _makeDateTimeStamp(self, entry):
+        dts = None
+        if 'published_parsed' in entry:
+            dts = entry.published_parsed
+        elif 'created_parsed' in entry:
+            dts = entry.created_parsed
+        if dts:
+            stamp = datetime.datetime(dts.tm_year, dts.tm_mon,
+                                      dts.tm_mday, dts.tm_hour,
+                                      dts.tm_min, dts.tm_sec)
+        else:
+            stamp = datetime.datetime.now()
+        return stamp
+
+
+    def _makeLocalName(self, url, entry):
+        stamp = self._makeDateTimeStamp(entry)
+        localName = os.path.join(self.targetDir,
+                                 self.renamer(url, entry, stamp))
+        while os.path.isfile(localName):
             localName = os.path.join(self.targetDir,
                                      self.renamer(url, entry, stamp))
-            while os.path.isfile(localName):
-                localName = os.path.join(self.targetDir,
-                                         self.renamer(url, entry, stamp))
-
-            r = requests.get(url, stream=True)
-            f = open(localName, 'wb')
-            for chunk in r.iter_content(chunk_size = 1024*4*4*4):
-                if chunk:
-                    f.write(chunk)
-            f.close()
-            self.fetched.append(url)
-            self.log.update(url)
+        return localName
