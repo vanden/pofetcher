ba324e5d8177a56f1b6b56b500f20a1a59b32483
Brian van den Broek <vanden@gmail.com>
2018-04-19 21:10:31 -0700

    Improve subscription logic

--- subscription.py
+++ subscription.py
@@ -1,20 +1,28 @@
+import datetime, os
+
+import feedparser, requests
+
 from config import podcastDir, podlog
 from renamer import Renamer
 
-import feedparser
 
 class Subscription():
 
     def __init__(self, feed=None, log=None, targetDir=podcastDir, count=5,
-                 renamer=None):
+                 renamer=None, name=None):
         self.feed = feed
         self.log = log
         self.targetDir = targetDir
         self.count = count
         self.renamer = renamer or Renamer().rename
+        self.name = name
+
+        # To prevent duplicate fetching which occurred with some feeds
+        self.fetched = []
 
 
     def parseFeed(self):
+        print(f"Parsing {self.name}")
         feed = feedparser.parse(self.feed)
         entriesToFetch = []
         for entry in feed['entries'][:self.count]:
@@ -26,11 +34,22 @@ class Subscription():
 
     def _fetch(self, entries):
         for entry in entries:
-
-            url = entry.enclosures[0].href
-            if url in self.log:
+            try:
+                url = entry.enclosures[0].href
+            except IndexError:
+                # It seems that this can occur when a podcast feed includes
+                # non-podcast items, too. Not sure what more if anything I
+                # should do. ThinkMore
                 continue
-
+            if url in self.log or url in self.fetched:
+                continue
+            if '?' in url:
+                realurl = url.split('?', 1)[0]
+                if not realurl.split('.')[-1] in ['mp3', 'ogg']:
+                    print(''.join(
+                        ["Don't know how to parse",
+                         f"{orig_url} from {self.name}; skipping"]))
+            print(f"\tFetching {url}")
             dts = None
             if 'published_parsed' in entry:
                 dts = entry.published_parsed
@@ -55,6 +74,5 @@ class Subscription():
                 if chunk:
                     f.write(chunk)
             f.close()
-
+            self.fetched.append(url)
             self.log.update(url)
-
